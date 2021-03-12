68e5213631c79626b58eccba77198cdd6ca6cb82
shashikiran-tatoju <shashikiran070@gmail.com>
2021-03-13 00:26:52 +0530

    Updates

---
+++ precache-manifest.37fda79aebffbdc217ca162441d8afaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc74802b03426f2531731f5069d41409",
+    "url": "/MultilayerForm/index.html"
+  },
+  {
+    "revision": "2c6bfbb79760151f2e3a",
+    "url": "/MultilayerForm/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "3c1881d06e3210ff3d6d",
+    "url": "/MultilayerForm/static/js/2.0e8a4728.chunk.js"
+  },
+  {
+    "revision": "2c6bfbb79760151f2e3a",
+    "url": "/MultilayerForm/static/js/main.b88467e5.chunk.js"
+  },
+  {
+    "revision": "0cb4d2e2c9cb6f9a3082",
+    "url": "/MultilayerForm/static/js/runtime~main.0550a16a.js"
+  }
+]);
